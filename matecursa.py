6d01f7784ab57ff1461b8b9f993af968baa2491d
jordiprats <jordi.prats@gmail.com>
2020-11-01 15:57:50 +0100

    sumes mes grosses

--- matecursa.py
+++ matecursa.py
@@ -45,9 +45,11 @@ def restes(file, pages, min, max):
 @click.command()
 @click.option('--file', default='sumes.pdf', help='output file')
 @click.option('--pages', default=30, help='number of pages')
-@click.option('--min', default=1, help='max int')
+@click.option('--min', default=1, help='min int')
 @click.option('--max', default=6, help='max int')
-def sumes(file, pages, min, max):
+@click.option('--min-segona-unitat', default=0, help='max int segona unitat')
+@click.option('--max-segona-unitat', default=0, help='max int segona unitat')
+def sumes(file, pages, min, max, min_segona_unitat, max_segona_unitat):
     pdf = FPDF(orientation='P', unit='mm', format='A4')
     for pagina in range(0, pages):
         pdf.add_page()
@@ -66,7 +68,15 @@ def sumes(file, pages, min, max):
                 while anterior_operacio == operacio:
                     print('range: '+str(min)+'-'+str(max))
                     primer_numero = randrange(min, max)
-                    segon_numero = randrange(min, max)
+                    if min_segona_unitat:
+                        min_segon_numero=min_segona_unitat
+                    else:
+                        min_segon_numero=min
+                    if max_segona_unitat:
+                        max_segon_numero=max_segona_unitat
+                    else:
+                        max_segon_numero=max
+                    segon_numero = randrange(min_segon_numero, max_segon_numero)
                     operacio = str(primer_numero)+' + '+str(segon_numero)+' ='
                 pdf.cell(w=0,h=0, txt=operacio, ln=0 )
                 anterior_operacio = operacio
