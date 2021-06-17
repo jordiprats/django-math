09d50c91587f6eeb7bd2d3978c331b8716fc53dc
jordiprats <jordi.prats@gmail.com>
2021-06-17 21:51:26 +0200

    multiplicacions

--- matecursa.py
+++ matecursa.py
@@ -87,6 +87,49 @@ def sumes(file, pages, min, max, min_segona_unitat, max_segona_unitat, multiplic
     pdf.output(file,'F')
 
 @click.command()
+@click.option('--file', default='multiplicacions.pdf', help='output file')
+@click.option('--pages', default=30, help='number of pages')
+@click.option('--min', default=1, help='min int')
+@click.option('--max', default=6, help='max int')
+@click.option('--min-segona-unitat', default=0, help='max int segona unitat')
+@click.option('--max-segona-unitat', default=0, help='max int segona unitat')
+@click.option('--multiplicador', default=1, help='multiplicador')
+def multiplicacions(file, pages, min, max, min_segona_unitat, max_segona_unitat, multiplicador):
+    pdf = FPDF(orientation='P', unit='mm', format='A4')
+    for pagina in range(0, pages):
+        pdf.add_page()
+        pdf.set_font('helvetica', '', 15.0)
+        pdf.set_xy(5.0, 18)
+        pdf.cell(w=0,h=0, txt='Nom: ........................................................................... Data: ...................................', ln=0 )
+        pdf.set_xy(5.0, 276)
+        pdf.set_font('helvetica', '', 25.0)
+        pdf.cell(w=0,h=0, txt='Operacions fetes en 2 minuts .......................', ln=0 )
+        pdf.set_font('helvetica', '', 20.0)
+        anterior_operacio = ''
+        operacio = ''
+        for columna in range(0, 4):
+            for linea in range(0,14):
+                pdf.set_xy(10.0 + (columna*50), 32.5+ (linea*17))
+                while anterior_operacio == operacio:
+                    print('range: '+str(min)+'-'+str(max))
+                    primer_numero = randrange(min, max)
+                    if min_segona_unitat:
+                        min_segon_numero=min_segona_unitat
+                    else:
+                        min_segon_numero=min
+                    if max_segona_unitat:
+                        max_segon_numero=max_segona_unitat
+                    else:
+                        max_segon_numero=max
+                    segon_numero = randrange(min_segon_numero, max_segon_numero)
+                    operacio = str(primer_numero*multiplicador)+' x '+str(segon_numero*multiplicador)+' ='
+                pdf.cell(w=0,h=0, txt=operacio, ln=0 )
+                anterior_operacio = operacio
+        if pagina%4 == 0:
+            max += 1
+    pdf.output(file,'F')
+
+@click.command()
 @click.option('--file', default='sumes_hortizontal.pdf', help='output file')
 @click.option('--pages', default=10, help='number of pages')
 @click.option('--min1', default=100, help='min int')
@@ -124,6 +167,7 @@ def sumes_horitzontal(file, pages, min1, max1, min2, max2, min3, max3):
 curses.add_command(sumes)
 curses.add_command(sumes_horitzontal)
 curses.add_command(restes)
+curses.add_command(multiplicacions)
 
 if __name__ == '__main__':
     curses()
